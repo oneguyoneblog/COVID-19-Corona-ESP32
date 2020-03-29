326e00fd647e0c4a2397565cc3119f5e74bd12b8
One Guy, One Blog <OneGuy-OneBlog@users.noreply.github.com>
2020-03-29 03:46:52 +0200

    Update COVID-19-Corona-ESP32.ino

--- COVID-19-Corona-ESP32.ino
+++ COVID-19-Corona-ESP32.ino
@@ -2,7 +2,7 @@
 
 	Get COVID-19 Corona REST API data with the ESP32 microcontroller
 
-	Code example; for more informatio see:
+	Code example; for more information see:
 	https://oneguyoneblog.com/2020/03/10/covid-19-corona-tracker-esp32-arduino-ide/
 
 
